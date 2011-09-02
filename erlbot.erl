681e8ac4d7130dfc0605876f011912fe4e921c2c
Daniel Kendell <daniel_kendell@macdan.net>
2011-09-02 18:33:06 +0100

    Split erlbot:start/0 into two. erlbot:start/0 will look for config in config.erl and erlbot:start/1 accepts a proplist of config.

--- erlbot.erl
+++ erlbot.erl
@@ -1,12 +1,15 @@
 -module( erlbot ).
--export( [ start/0 ] ).
+-export( [ start/0, start/1 ] ).
 
 start() ->
-	{ ok, Config } = file:consult( "config.erl" ),
-	
+	case file:consult( "config.erl" ) of
+		{ ok, Config } -> start( Config );
+		Err            -> io:format( "Can't read config. ~p~n", [ Err ] )
+	end.
+
+start( Config ) ->
 	{ host, Host } = proplists:lookup( host, Config ),
 	{ nick, Nick } = proplists:lookup( nick, Config ),
 	{ owner, Owner } = proplists:lookup( owner, Config ),
 	
-	{ ok, Pid } = bot:start_link( Host, Nick, Owner ),
-	Pid. 
+	bot:start_link( Host, Owner, Nick ).

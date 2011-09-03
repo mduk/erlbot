97f373bf4c6293ed28ce387674b2dd8d856a6682
Daniel Kendell <daniel_kendell@macdan.net>
2011-09-03 13:38:08 +0100

    Revert 7803e24c2c193b645bd2c0848eef9c675cd55cb1^..HEAD

--- config.erl
+++ config.erl
@@ -1,11 +1,3 @@
 { host, { "irc.appliedirc.com", 6667 } }.
 { nick, "erlbot" }.
 { owner, "macdan" }.
-
-{ channels, [
-
-	{ "#erl2", [
-		{ plugins, [ foobar ] }
-	] }
-
-] }.
--- erlbot.erl
+++ erlbot.erl
@@ -12,17 +12,4 @@ start( Config ) ->
 	{ nick, Nick } = proplists:lookup( nick, Config ),
 	{ owner, Owner } = proplists:lookup( owner, Config ),
 	
-	{ ok, Bot } = bot:start_link( Host, Owner, Nick ),
-	
-	case proplists:lookup( channels, Config ) of
-		none            -> nevermind;
-		{ _, Channels } -> spawn( ?MODULE, start_channels, [ Bot, Channels ] )
-	end,
-	
-	Bot.
-
-start_channels( Bot, Channels ) ->
-	timer:sleep( 3000 ),
-	lists:foreach( fun( Channel ) ->
-		bot:join( Bot, Channel )
-	end, Channels ).
+	bot:start_link( Host, Owner, Nick ).
